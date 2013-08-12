8951663c24b9aca1273b193cf9c08c6c5f871ef4
fedind <fedind@gmail.com>
2013-08-12 22:44:18 +0400

    csv like output for benchmark script

--- src/main/runners/benchmark.groovy
+++ src/main/runners/benchmark.groovy
@@ -4,18 +4,21 @@ result.delete()
 def files = ['\\temp\\text.dat']
 result << "files = $files\n"
 for(inputFile in files){
-    result << inputFile << "\n" << inputFile.length() / 1024*1024 << "MB\n"
+    result << inputFile << "\n" << inputFile.length() << "B\n"
+    result << "threads number" << "\t" << "elapsed time" << "\n"
     println inputFile
     for(i in 10..1){
-        println "number of threads: $i"
-        result << "number of threads: $i\n"
-        def start = System.nanoTime()
-        def cmd = """esort.bat $inputFile $i \\temp\\out${i}.dat"""
-        println cmd
-        result << cmd << "\n"
-        def proc = cmd.execute()
-        proc.waitFor()
-        result << "File: $inputFile\n" << "Threads number: $i\n" << "Elapsed time: ${(System.nanoTime() - start)/1000000}ms\n\n"
+        10.times {
+            println "number of threads: $i"
+            result << "$i\t"
+            def start = System.nanoTime()
+            def cmd = """esort.bat $inputFile $i \\temp\\out${i}.dat"""
+            println cmd
+            def proc = cmd.execute()            
+            proc.waitFor()
+            result << "${(System.nanoTime() - start)/1000000}ms\n"
+            println proc.text
+        }        
     }
 }
 println 'Finished'
